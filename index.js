9ba2ab562eebda91d5d54eaa4e420b87365cd9c1
Diksha Yadav <yadav.diksha1503@gmail.com>
2023-04-23 22:11:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 let express = require('express');
 let app = express();
 let http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
+let connections = [];
 
+io.on("connect", (socket) => {
+  
+  connections.push(socket);
+  console.log(`${socket.id} has connected`);
+
+  socket.on("draw", (data) =>{
+    connections.forEach((con) => {
+      if(con.id !== socket.id){
+        con.emit('ondraw',{x : data.x , y : data.y})
+      }
+    })
+  })
+
+  socket.io("down" , (data) => {
+    connections.forEach((con) => {
+      if(con.id !== socket.id){
+        con.emit('ondown',{x : data.x , y : data.y})
+      }
+    })
+  })
+
+  socket.on("disconnect", (reason) => {
+    console.log(`${socket.id} is disconnected`);
+    connections = connections.filter((con) => con.id !== socket.id);
+  });
+});
+
+io.on('connection', (socket) => {
+  console.log('Connected...')
+  socket.on('message', (msg) => {
+      socket.broadcast.emit('message', msg)
+  })
+})
 
 app.use(express.static("public"));
 
@@ -12,11 +47,3 @@ http.listen(PORT, () => {
 });
 
 
-const io = require('socket.io')(http);
-io.on('connection', (socket) => {
-  console.log('Connected...')
-  socket.on('message', (msg) => {
-      socket.broadcast.emit('message', msg)
-  })
-
-})
