7d4fa44b44a690b54ef028e90d25c2a5138bc2e3
Ayush Kumar <72346028+ayushh02@users.noreply.github.com>
2023-06-10 00:07:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ io.on("connect", (socket) => {
     })
   })
 
-  socket.io("down" , (data) => {
+  socket.on("down" , (data) => {
     connections.forEach((con) => {
       if(con.id !== socket.id){
         con.emit('ondown',{x : data.x , y : data.y})
@@ -26,24 +26,41 @@ io.on("connect", (socket) => {
     })
   })
 
+  socket.on('message', (msg) => {
+    socket.broadcast.emit('message', msg)
+})
+
+socket.on('correct', (crct) => {
+  socket.broadcast.emit('correct', crct)
+})
+
+socket.on('h1', (w) => {
+  socket.broadcast.emit('h1',w)
+})
+
+socket.on('time', (timeSecond) => {
+  socket.broadcast.emit('time', timeSecond)
+})
+
+socket.on('score', (scr) => {
+  socket.broadcast.emit('score',scr)
+})
+
+socket.on('update', (y) => {
+  socket.broadcast.emit('update',y)
+})
+
+
   socket.on("disconnect", (reason) => {
     console.log(`${socket.id} is disconnected`);
     connections = connections.filter((con) => con.id !== socket.id);
   });
 });
 
-io.on('connection', (socket) => {
-  console.log('Connected...')
-  socket.on('message', (msg) => {
-      socket.broadcast.emit('message', msg)
-  })
-})
-
 app.use(express.static("public"));
 
-let PORT = process.env.YOUR_PORT || process.env.PORT || 8090;
+let PORT = process.env.YOUR_PORT || process.env.PORT || 8050;
 http.listen(PORT, () => {
     console.log('server started on port'+ PORT);
 });
-
 
